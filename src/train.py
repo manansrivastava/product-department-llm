691e8f802e41c9b2ed7be9a55cd55a438d5fd2b7
manansrivastava <manansrivastava01@gmail.com>
2026-01-11 14:48:33 +0530

    Fix SFTTrainer init for latest TRL2

--- src/train.py
+++ src/train.py
@@ -1,17 +1,11 @@
 import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments
+from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments
 from trl import SFTTrainer
 from peft import LoraConfig
 from prepare_data import load_and_prepare_data
 
 MODEL_NAME = "ybelkada/falcon-7b-sharded-bf16"
 OUTPUT_DIR = "results"
-
-PER_DEVICE_BATCH_SIZE = 2
-GRADIENT_ACCUMULATION_STEPS = 8
-MAX_SEQ_LENGTH = 512
-MAX_STEPS = 120
-LEARNING_RATE = 2e-4
 
 
 def train():
@@ -41,25 +35,22 @@ def train():
             "query_key_value",
             "dense",
             "dense_h_to_4h",
-            "dense_4h_to_h"
-        ]
+            "dense_4h_to_h",
+        ],
     )
 
     training_args = TrainingArguments(
         output_dir=OUTPUT_DIR,
-        per_device_train_batch_size=PER_DEVICE_BATCH_SIZE,
-        gradient_accumulation_steps=GRADIENT_ACCUMULATION_STEPS,
-        learning_rate=LEARNING_RATE,
-        max_steps=MAX_STEPS,
+        per_device_train_batch_size=2,
+        gradient_accumulation_steps=8,
+        learning_rate=2e-4,
+        max_steps=120,
         fp16=True,
         logging_steps=1,
         save_steps=20,
         save_total_limit=2,
-        warmup_ratio=0.03,
-        lr_scheduler_type="constant",
         optim="adamw_torch",
         report_to="none",
-        group_by_length=True
     )
 
     trainer = SFTTrainer(
@@ -67,12 +58,7 @@ def train():
         train_dataset=dataset["train"],
         peft_config=peft_config,
         args=training_args,
-        max_seq_length=MAX_SEQ_LENGTH
     )
-
-    for name, module in trainer.model.named_modules():
-        if "norm" in name:
-            module.to(torch.float32)
 
     trainer.train()
 
